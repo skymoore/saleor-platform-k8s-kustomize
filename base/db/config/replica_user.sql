c0f88a91b23c8e14d1649fa26afb75249edef6f0
Sky Moore <i@msky.me>
2022-11-21 15:30:13 -0600

    some updates

--- base/db/config/replica_user.sql
+++ base/db/config/replica_user.sql
@@ -1,7 +1,9 @@
 -- Script being executed on DB init, creates read only user
 -- for replicas purposes.
 \set saleor_ro_password '\'' :SALEOR_RO_PASSWORD '\''
-CREATE USER saleor_read_only WITH PASSWORD :saleor_ro_password;
+CREATE USER saleor_read_only WITH PASSWORD 'saleor';
+-- ALTER USER saleor_read_only WITH PASSWORD :saleor_ro_password;
+ALTER USER saleor_read_only WITH PASSWORD 'saleor';
 GRANT CONNECT ON DATABASE :POSTGRES_DB TO saleor_read_only;
 GRANT USAGE ON SCHEMA public TO saleor_read_only;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO saleor_read_only;
